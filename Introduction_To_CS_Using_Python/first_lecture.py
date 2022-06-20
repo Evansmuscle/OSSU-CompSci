f4333c3f0ebb9a5bf412f66a3be501bd74c72880
Yiğit Kaan Korkmaz <77935633+Evansmuscle@users.noreply.github.com>
2022-06-20 12:14:39 +0300

    feat: First lecture

---
+++ Introduction_To_CS_Using_Python/first_lecture.py
@@ -0,0 +1,16 @@
+# import random
+
+# winner = random.randint(1, 271)
+
+# print(winner)
+
+def findSqrt(x=int):
+    if x < 0:
+      return
+  
+    return x**0.5
+
+square_root = findSqrt(32)
+square_root = findSqrt(16)
+
+print(square_root)
